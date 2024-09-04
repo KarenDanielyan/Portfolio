0ce0ed9571ceea1b41563898b070a68fbec8d69f
Karen Danielyan <karendanielyan66@gmail.com>
2024-09-04 18:14:38 +0400

    About and Hero sections complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import styles from './App.module.css'
 import Navbar from "./Components/Navbar/Navbar.jsx";
+import Hero from "./Components/Hero/Hero.jsx";
+import About from "./Components/About/About.jsx";
 
 function App() {
   return (
     <>
       <div className={styles.App}>
           <Navbar />
+          <Hero />
+          <About />
       </div>
     </>
   )
---
+++ frontend/src/Components/About/About.jsx
@@ -0,0 +1,41 @@
+import styles from './About.module.css';
+import {getAssetURL} from "../../utils.js";
+
+function About() {
+    return (
+        <section id='about' className={styles.container}>
+            <h2 className={styles.title}>About</h2>
+            <div className={styles.content}>
+                <video className={styles.aboutVideo} controls>
+                    <source src={getAssetURL("about/video.mp4")} type="video/mp4"/>
+                    Your browser does not support the video tag!
+                </video>
+                <ul className={styles.aboutItems}>
+                    <li className={styles.aboutItem}>
+                        <img src={getAssetURL("about/info1.png")} alt="About 1"/>
+                        <div>
+                            <h3>Dummy Header</h3>
+                            <p>Dummy Text</p>
+                        </div>
+                    </li>
+                    <li className={styles.aboutItem}>
+                        <img src={getAssetURL("about/info2.png")} alt="About 2"/>
+                        <div>
+                            <h3>Education</h3>
+                            <p>Put your education here.</p>
+                        </div>
+                    </li>
+                    <li className={styles.aboutItem}>
+                        <img src={getAssetURL("about/info3.png")} alt="About 3"/>
+                        <div>
+                            <h3>Work in Progress</h3>
+                            <p>Write some fancy LinkedIn style text.</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    )
+}
+
+export default About;
