bbd253d64cac1804bfbdc2ed70e8e51df6832a59
Karen Danielyan <karendanielyan66@gmail.com>
2024-09-20 02:47:44 +0400

    Contacts now fetch data from the backend

--- frontend/src/Components/Contacts/Contacts.jsx
+++ frontend/src/Components/Contacts/Contacts.jsx
@@ -1,7 +1,23 @@
 import styles from './Contacts.module.css';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function Contacts() {
-	const mail = 'mail@mail.com';
+	const [data, setData] = useState(null);
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	const fetchData = () => {
+		axios.get("http://localhost:8000/api/contacts/").then(
+			(response) => {
+			setData(response.data);
+			}
+		).catch(error => {
+			console.log(error);
+		});
+	}
 
 	return (
 		<footer id='contacts' className={styles.container}>
@@ -12,15 +28,15 @@ function Contacts() {
 			<ol className={styles.links}>
 				<li className={styles.link}>
 					<img src={'/contacts/mail.svg'} alt={'email'}/>
-					<a href={`mailto:${mail}`}>{mail}</a>
+					<a href={`mailto:${data?.email}`}>{data?.email}</a>
 				</li>
 				<li className={styles.link}>
 					<img src={'/contacts/github.svg'} alt={'github'}/>
-					<a href={'https://github.com/KarenDanielyan'}>Github</a>
+					<a href={data?.github}>Github</a>
 				</li>
 				<li className={styles.link}>
 					<img src={'/contacts/linkedin.svg'} alt={'linkedin'}/>
-					<a href={'https://www.linkedin.com/in/karen-danielyan-723812124'}>LinkedIn</a>
+					<a href={data?.linkedin}>LinkedIn</a>
 				</li>
 			</ol>
 		</footer>
